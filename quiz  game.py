5bb4c804825f727c24ec735ffb0c68d0d6d99ef6
Aditya Mishra <93374202+adityamishra123@users.noreply.github.com>
2022-03-11 20:40:43 +0530

    Add files via upload

---
+++ quiz  game.py
@@ -0,0 +1,25 @@
+def check_guess(guess,answer):
+    global score
+    still_guessing=True
+    attempt=0
+    while still_guessing and attempt<3:
+        if guess.lower()==answer.lower():
+            print("correct answer")
+            score=score+1
+            still_guessing=False
+        else:
+            if attempt<2:
+                guess=input("sorry wrong answer. try again")
+                attempt=attempt+1
+    if attempt==3:
+        print("the correct answer is ",answer)
+
+score=0
+print("Guess the animal")
+guess1=input("which bear lives at the north pole?")
+check_guess(guess1,"polar bear")
+guess2=input("which is the fastest land animal ? ")
+check_guess(guess2,"cheetah")
+guess3=input("which is the largest animal? ")
+check_guess(guess3,"blue whale")
+print("your score is"+str(score) )
